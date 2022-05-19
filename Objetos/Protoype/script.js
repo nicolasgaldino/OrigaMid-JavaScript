ac0844c40d81284a6300cec68e556de60864b443
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-05-19 11:42:30 -0300

    Exercícios finalizados.

--- Objetos/Protoype/script.js
+++ Objetos/Protoype/script.js
@@ -91,18 +91,41 @@ Carro.marca.charAt(0) // String
 
 // Crie uma função construtora de Pessoas deve conter nome, sobrenome e idade crie um método no protótipo que retorne o nome completo da pessoa
 
+function Pessoa(nome, sobrenome, idade) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+}
+
+Pessoa.prototype.nomeCompleto = function() {
+  return `${this.nome} ${this.sobrenome}`;
+};
+
+Pessoa.prototype.dados = function() {
+  return `${this.nome} ${this.sobrenome} tem ${this.idade} anos.`;
+};
+
+const nicolasGal = new Pessoa('Nícolas', 'Galdino', 23);
+
+console.log(nicolasGal.dados());
+console.log(nicolasGal.nomeCompleto());
+
 // Liste os métodos acessados por dados criados com NodeList, HTMLCollection, Document
+
+console.log(NodeList.prototype);
+console.log(HTMLCollection.prototype);
+console.log(Document.prototype);
 
 // Liste os construtores dos dados abaixo
 const li = document.querySelector('li');
 
-li;
-li.click;
-li.innerText;
-li.value;
-li.hidden;
-li.offsetLeft;
-li.click();
+li; // HTMLLIElement
+li.click; // Function
+li.innerText; // String
+li.value; // Number
+li.hidden; // Boolean
+li.offsetLeft; // Number
+li.click(); // Undefined
 
 // Qual o construtor do dado abaixo:
-li.hidden.constructor.name;
+li.hidden.constructor.name; // String
